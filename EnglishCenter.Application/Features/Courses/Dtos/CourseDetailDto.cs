f5f9c3e3e81cece0b033927c730c47a351c55343
huynqhe186195 <166681804+huynqhe186195@users.noreply.github.com>
2026-04-16 12:16:48 +0700

    Commit New

---
+++ EnglishCenter.Application/Features/Courses/Dtos/CourseDetailDto.cs
@@ -0,0 +1,17 @@
+﻿namespace EnglishCenter.Application.Features.Courses.Dtos;
+
+public class CourseDetailDto
+{
+    public long Id { get; set; }
+    public string CourseCode { get; set; } = string.Empty;
+    public string Name { get; set; } = string.Empty;
+    public string? Description { get; set; }
+    public string? Level { get; set; }
+    public int? AgeMin { get; set; }
+    public int? AgeMax { get; set; }
+    public int TotalSessions { get; set; }
+    public decimal DefaultFee { get; set; }
+    public int Status { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime? UpdatedAt { get; set; }
+}
---
+++ EnglishCenter.Application/Features/Courses/Mappings/CourseProfile.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using EnglishCenter.Application.Features.Courses.Dtos;
+using EnglishCenter.Domain.Models;
+
+namespace EnglishCenter.Application.Features.Courses.Mappings;
+
+public class CourseProfile : Profile
+{
+    public CourseProfile()
+    {
+        CreateMap<Course, CourseDto>();
+        CreateMap<Course, CourseDetailDto>();
+        CreateMap<CreateCourseRequestDto, Course>();
+
+        CreateMap<UpdateCourseRequestDto, Course>()
+            .ForMember(dest => dest.Id, opt => opt.Ignore())
+            .ForMember(dest => dest.CourseCode, opt => opt.Ignore())
+            .ForMember(dest => dest.CreatedAt, opt => opt.Ignore())
+            .ForMember(dest => dest.IsDeleted, opt => opt.Ignore());
+    }
+}
