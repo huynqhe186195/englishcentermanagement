27e88a760bdf400691f80b4067715e046032b7dd
huynqhe186195 <166681804+huynqhe186195@users.noreply.github.com>
2026-04-21 11:43:29 +0700

    Load courses page data from API instead of hardcoded cards

--- EnglishCenter.Web/Pages/Courses/Index.cshtml.cs
+++ EnglishCenter.Web/Pages/Courses/Index.cshtml.cs
@@ -1,10 +1,27 @@
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using EnglishCenter.Web.Models;
+using EnglishCenter.Web.Services;
 
 namespace EnglishCenter.Web.Pages.Courses;
 
 public class IndexModel : PageModel
 {
-    public void OnGet()
+    private readonly IApiClient _apiClient;
+
+    public IndexModel(IApiClient apiClient)
     {
+        _apiClient = apiClient;
+    }
+
+    public List<CourseDto> Courses { get; set; } = new();
+
+    public async Task OnGetAsync()
+    {
+        var pagedCourses = await _apiClient.GetAsync<PagedResult<CourseDto>>(
+            "courses?PageNumber=1&PageSize=12&SortBy=CreatedAt&SortDirection=desc");
+
+        Courses = pagedCourses?.Items
+            .Where(x => x.Status == 1)
+            .ToList() ?? new List<CourseDto>();
     }
 }
